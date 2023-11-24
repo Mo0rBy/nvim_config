1fb718462c300f8b5260eb4c2163f58ea06c5cb5
Mo0rBy <will.moorby2@gmail.com>
2023-11-24 19:34:15 +0000

    Added nightfox color theme and moved essential Lazy plugins into lua/plugins/init.lua

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -16,8 +16,4 @@ vim.opt.rtp:prepend(lazypath)
 -- This is required to initialise Lazy
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-})
+require("lazy").setup("plugins")
