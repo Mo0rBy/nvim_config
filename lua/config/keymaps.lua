6a97b0c4e00f83c754e03d40ed427561057236bf
Mo0rBy <will.moorby2@gmail.com>
2023-11-26 16:57:16 +0000

    Added comment.lua, still need to set keymaps correctly

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,12 @@ keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
+
+
+
+  keymap.set("v", "<", "<gv")
+  keymap.set("v", ">", ">gv")
+
+  -- Comments
+  vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false }) -- Toggle Minimize
+  vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false }) -- Toggle Minimize
