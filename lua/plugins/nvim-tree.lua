dc772de13fab96ccfeb74d5fe172c354986368d0
wmoorby <wmoorby@deloitte.co.uk>
2024-01-11 10:58:47 +0000

    Moved nvim-web-devicons into nvim-tree config as a dependency

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,11 +4,11 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true})
 keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true})
 
-
-
-
 return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
   config = {},
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  }
 }
