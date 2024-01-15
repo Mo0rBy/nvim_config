feec93c8d8b2762137896e6845ff1590a7868e3e
wmoorby <wmoorby@deloitte.co.uk>
2024-01-15 17:48:03 +0000

    Added some basic keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,20 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+local keymap = vim.keymap
+
+-- general keymaps
+keymap.set("n", "x", '"_x') -- set 'x' in normal to NOT copy to register
+
+keymap.set("n", "<leader>+", "<C-a>") -- set keymap to increment numbers up
+keymap.set("n", "<leader>-", "<C-x>") -- set keymap to increment numbers down
+
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal length/height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
