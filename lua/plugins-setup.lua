9821617b6f5f8bc194e7edec89156383d16d7900
wmoorby <wmoorby@deloitte.co.uk>
2024-01-15 17:17:53 +0000

    Added monokai colorscheme

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup()
+local plugins = {
+    require("core.colorscheme") 
+}
+
+require("lazy").setup(plugins)
