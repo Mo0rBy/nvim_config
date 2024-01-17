f820abe51417c19db6ee0b68e72ad68ac9fee1da
wmoorby <wmoorby@deloitte.co.uk>
2024-01-17 09:06:49 +0000

    Added nvim-tree plugin for a nicer file explorer

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This neovim config is used by myself on a daily basis. This config uses lazy.nvi
 
 ## Plugins
 
-- [Lazy package manager](https://github.com/folke/lazy.nvim) (this plugin manager is required in order to install the remaining plugins)
-- [Monokai colorscheme](https://github.com/tanvirtin/monokai.nvim)
+- [Lazy package manager](https://github.com/folke/lazy.nvim) - this plugin manager is required in order to install the remaining plugins
+- [Monokai colorscheme](https://github.com/tanvirtin/monokai.nvim) - my preferred colorscheme, use a different one if you want to
+- [nvim-tree](https://github.com/nvim-tree/nvim-tree.lua) - a file explorer plugin, I prefer it over the default netrw plugin
+
--- lua/moorby/plugins/nvim-tree.lua
+++ lua/moorby/plugins/nvim-tree.lua
@@ -1,6 +1,60 @@
 return {
-    "https://github.com/nvim-tree/nvim-tree.lua",
-    filters = {
-        dotfiles = true,
-    },
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local nvimtree = require("nvim-tree")
+
+    -- recommended settings from nvim-tree documentation
+    -- disables the default netrw file explorer
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    -- configure nvim-tree
+    nvimtree.setup({
+      hijack_cursor = true, 
+      view = {
+        width = 40,
+        number = true,
+        relativenumber = true,
+        cursorline = true,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        highlight_git = true,
+        icons = {
+        git_placement = "after", 
+          glyphs = {
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
+            }
+          } 
+        }
+      },
+      filters = {
+          dotfiles = false, -- show dotfiles
+          git_ignored = false -- show files ignored by git
+      },
+      git = {
+        enable = true,
+      },
+      tab = {
+        sync = { -- sync open and closing nvim-tree across tabs
+          open = true,
+          close = true,
+        }
+      }
+    })
+
+    -- set keymaps (setting them here makes them load with the plugin itself and not before)
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+  end,
 }
