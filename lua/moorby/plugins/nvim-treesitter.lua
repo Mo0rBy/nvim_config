dfca61458c52abfa705b296311992c54059e7dd4
wmoorby <wmoorby@deloitte.co.uk>
2024-01-17 14:33:27 +0000

    Modified nvim-treesitter configuration

--- lua/moorby/plugins/nvim-treesitter.lua
+++ lua/moorby/plugins/nvim-treesitter.lua
@@ -1,7 +1,21 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  ensure_installed = "maintained", -- ensures that all maintained language parsers are installed
-  indent = {
-    enable = true,
-  }
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true }, -- uses dependency "windwp/nvim-ts-autotag"
+      ensure_installed = "all" -- ensures that all language parsers are installed
+    })
+
+    -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+    require('ts_context_commentstring').setup {}
+  end,
 }
