d38cd829ce23ba638a43a6528c1b345b82251a31
wmoorby <wmoorby@deloitte.co.uk>
2024-01-17 13:36:15 +0000

    Added nvim-treesitter plugin for better syntax highlighting

---
+++ lua/moorby/plugins/nvim-treesitter.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  ensure_installed = "maintained", -- ensures that all maintained language parsers are installed
+  indent = {
+    enable = true,
+  }
+}
