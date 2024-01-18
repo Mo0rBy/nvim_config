fd37cb4201842195ab841502311ab5df514ff4eb
wmoorby <wmoorby@deloitte.co.uk>
2024-01-18 13:35:52 +0000

    Added keymap for ToggleGitBlame in gitsigns plugin

--- lua/moorby/plugins/gitsigns.lua
+++ lua/moorby/plugins/gitsigns.lua
@@ -2,8 +2,12 @@ return {
   "lewis6991/gitsigns.nvim",
   lazy = false,
   config = function()
-    require("gitsigns").setup {
-      current_line_blame = true,
-    }
+    local keymap = vim.keymap
+
+    -- set keymap to toggle git blame
+    -- if git blame is left on, it moves LSP notifications further to the right
+    keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+
+    require("gitsigns").setup{}
   end
 }
