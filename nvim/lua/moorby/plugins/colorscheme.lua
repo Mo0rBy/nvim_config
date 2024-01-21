167ef3d3b5be63fcee1778a2bbd639c4479dc3dd
Mo0rBy <will.moorby2@gmail.com>
2024-01-21 22:31:51 +0000

    Switching to tokyonight colorscheming

--- nvim/lua/moorby/plugins/colorscheme.lua
+++ nvim/lua/moorby/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-  "bluz71/vim-nightfly-colors",
-  name = "nightfly",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  opts = {},
   config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme nightfly]])
-  end,
+    vim.cmd[[colorscheme tokyonight-night]]
+  end
 }
