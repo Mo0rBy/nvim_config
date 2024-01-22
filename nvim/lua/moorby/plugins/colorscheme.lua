4db1c68635361ab602ba0c6dd5565d5e347298b7
wmoorby <wmoorby@deloitte.co.uk>
2024-01-22 19:42:55 +0000

    Added custom git and gitSigns colors to tokyonight colorscheme configuration

--- nvim/lua/moorby/plugins/colorscheme.lua
+++ nvim/lua/moorby/plugins/colorscheme.lua
@@ -4,6 +4,30 @@ return {
   priority = 1000,
   opts = {},
   config = function()
-    vim.cmd("colorscheme tokyonight-night")
+    require("tokyonight").setup({
+      style = "night", -- set the 'night' style
+
+      -- set custom colors
+      on_colors = function(colors)
+        local gitDeleteRed = "#D41501"
+
+        -- colors for gitSigns plugin
+        colors.gitSigns = {
+          add = colors.green,
+          change = colors.orange,
+          delete = gitDeleteRed
+        }
+
+        -- colors for git
+        colors.git = {
+          add = colors.green,
+          change = colors.orange,
+          delete = gitDeleteRed
+        }
+      end
+
+    })
+
+    vim.cmd("colorscheme tokyonight")
   end
 }
