7883517d60a86b8618e3e1b47775fcce25df9865
wmoorby <wmoorby@deloitte.co.uk>
2024-03-02 15:40:02 +0000

    Added indent-rainbowline dependency to indent-blankline plugin with rainbow-delimiters integration

--- nvim/lua/moorby/plugins/indent-blankline.lua
+++ nvim/lua/moorby/plugins/indent-blankline.lua
@@ -1,23 +1,43 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {},
   event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
   config = function()
-    local highlight = { "iblScopeColor" }
-
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
     local hooks = require("ibl.hooks")
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "iblScopeColor", { fg = "#ae81ff" })
-    end)
 
-    require("ibl").setup({
-      scope = {
-        enabled = true,
-        highlight = highlight,
-      },
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { link = "RainbowDelimiterRed" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { link = "RainbowDelimiterYellow" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { link = "RainbowDelimiterBlue" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { link = "RainbowDelimiterOrange" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { link = "RainbowDelimiterGreen" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { link = "RainbowDelimiterViolet" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { link = "RainbowDelimiterCyan" })
+    end)
+    vim.g.rainbow_delimiters = { highlight = highlight }
+
+    local opts = require("indent-rainbowline").make_opts({}, {
+      color_transparency = 0.15,
     })
 
-  hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    opts.scope = {
+      highlight = highlight
+    }
+
+    require("ibl").setup(opts)
+
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end
 }
