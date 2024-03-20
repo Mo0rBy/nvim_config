9da7074d4ae5a0ab8bd8df53b3c33b6782ca50b5
wmoorby <wmoorby@deloitte.co.uk>
2024-03-20 08:49:07 +0000

    Adding basic keymaps to neogit plugin configuration. Updated git blame keymaps. Updated Telescope with a git branch keymap. Updated diffview keymaps.

--- nvim/lua/moorby/plugins/blame.lua
+++ nvim/lua/moorby/plugins/blame.lua
@@ -7,9 +7,14 @@ return {
   "FabijanZulj/blame.nvim",
   lazy = false,
   config = function()
+    -- Setup plugin
     require("blame").setup({
       merge_consecutive = true,
       date_format = "%d/%m/%Y %H:%M"
     })
+
+    -- Setup keymaps
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>gfb", ":ToggleBlame<CR>", {desc = "Toggle git blame (entire file)"})
   end
 }
