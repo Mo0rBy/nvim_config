c2e4a3c146b49a599f36b61c17112c562a8a7e43
wmoorby <wmoorby@deloitte.co.uk>
2024-03-20 08:17:28 +0000

    Adding neogit plugin to nvim configuration

---
+++ nvim/lua/moorby/plugins/neogit.lua
@@ -0,0 +1,27 @@
+return {
+  "NeogitOrg/neogit",
+  event="VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim", -- optional
+    -- "ibhagwan/fzf-lua",              -- optional
+  },
+  config = function()
+    local neogit = require("neogit")
+
+    neogit.setup({
+      git_services = { -- Used to generate URL's for branch popup action "pull request".
+        ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+        ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+        ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+        -- Maybe add Gitea here
+      },
+      disable_line_numbers = true,
+      kind = "split",
+
+    })
+  end
+}
