0b65f801825eb443d93cd8f747e9cf9a3e4b50ec
Mo0rBy <will.moorby2@gmail.com>
2024-07-05 17:25:20 +0100

    Added nightfox color theme and moved essential Lazy plugins into lua/plugins/init.lua

---
+++ lua/plugins/init.lua
@@ -0,0 +1,6 @@
+-- Initialise these plugins in init.lua as they are closely tied to Lazy, and are very useful for setting nvim config
+return {
+	"folke/which-key.nvim",
+  	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+  	"folke/neodev.nvim",
+}
