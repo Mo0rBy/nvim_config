69e78a30cff36f7b5b3be664bff7e1a0af027a33
Mo0rBy <will.moorby2@gmail.com>
2024-07-05 17:25:25 +0100

    Added a comment to blame.lua distinguishing between full file git blame and single line git blame

--- nvim/lua/moorby/plugins/blame.lua
+++ nvim/lua/moorby/plugins/blame.lua
@@ -1,3 +1,8 @@
+-- This git blame plugin provides a nice blame for the entire file (all lines):
+-- 1. In a separate window to the side of the file
+-- 2. As virtual text within the file
+
+-- For individual lines (or current line) git blames, see the git-signs.lua config
 return {
   "FabijanZulj/blame.nvim",
   lazy = false,
