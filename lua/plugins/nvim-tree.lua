140c7e822d6c1f362d423f6ca6555a3a126e5a86
Mo0rBy <will.moorby2@gmail.com>
2024-07-05 17:25:20 +0100

    Added nvim-tree file explorer keymaps

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,12 @@
+local keymap = vim.keymap
+
+-- Directory Navigation
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true})
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true})
+
+
+
+
 return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
