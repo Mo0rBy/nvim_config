58fa1039a53b079ebb1012318e3d764585863209
Mo0rBy <will.moorby2@gmail.com>
2024-08-04 10:54:33 +0100

    Update lualine config to show @recording messages (noice plugin messes with it somehow)

--- nvim/lua/moorby/plugins/lualine.lua
+++ nvim/lua/moorby/plugins/lualine.lua
@@ -8,7 +8,14 @@ return {
       sections = {
         lualine_c = {
           "buffers"
-        }
+        },
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          }
+        },
       }
     }
   end
